6735b6503fc3c279d31a3c035ca2da041018217a
juftfi <fourswapxyz@gmail.com>
2026-03-01 14:55:08 +0700

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -156,14 +156,14 @@ const Sidebar = ({ visible, setVisible }: any) => {
                 onClick={() => {
                   setVisible(false);
                 }}
-              href={"https://x.com/ClawSceen"}
+              href={"https://x.com/ClawSceen" target="_blank"}
                 className={`p-2 rounded-md text-xl text-white font-bold ${
                   router.pathname === "/Nft"
                     ? "bg-gray-800 w-full text-white  "
                     : ""
                 }`}
               >
-                Community
+                X
               </Link>
             </div>
 
